1bda549d0c6108a1ccaacd00580c640f50c4fa08
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-11-27 19:20:39 -0600

    compelte integration test

--- src/main/java/com/codeup/blog/blog/repositories/PostRepository.java
+++ src/main/java/com/codeup/blog/blog/repositories/PostRepository.java
@@ -1,10 +1,11 @@
 package com.codeup.blog.blog.repositories;
 
 import com.codeup.blog.blog.models.Post;
+import com.codeup.blog.blog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    Post findByTitle(String title);
 }
 
 
