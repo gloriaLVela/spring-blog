3c905a2f8765bfe1551c1e9c008ed2e95fee254a
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-12-13 12:55:43 -0600

    add picture to blog

--- src/main/java/com/codeup/blog/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/blog/controllers/HomeController.java
@@ -1,15 +1,28 @@
 package com.codeup.blog.blog.controllers;
 
+import com.codeup.blog.blog.models.Post;
+import com.codeup.blog.blog.repositories.PostRepository;
+import com.codeup.blog.blog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index(){
 
+    private final UserRepository userDao;
+
+    public HomeController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/")
+    public String index(Model viewModel) {
+        viewModel.addAttribute("users", userDao.findAll());
         return "home";
     }
 }
