3d9ae7ed8ac25daf9937412cd208fa53e5863176
gloriaLVela <37721059+gloriaLVela@users.noreply.github.com>
2020-01-19 18:42:52 -0600

    Revert "remove commented code and organize css"

--- src/main/java/com/codeup/blog/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/blog/controllers/HomeController.java
@@ -1,12 +1,18 @@
 package com.codeup.blog.blog.controllers;
 
+import com.codeup.blog.blog.models.Post;
+import com.codeup.blog.blog.repositories.PostRepository;
 import com.codeup.blog.blog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class HomeController {
+
 
     private final UserRepository userDao;
 
